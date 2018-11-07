7d5aa92f3bd5fa870781bb25750cbefa51a72bf6
trinilime <trinilime123@live.com>
2018-11-07 18:27:29 -0500

    take a string and return it in Pig Latin

---
+++ PigLatin/PigLatin/PigLatin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatin
+{
+    class PigLatin
+    {
+        static void Main(string[] args)
+        {
+            string outputString = "Please enter a word to be converted to pig Latin. Type \"exit\" to leave the program.";
+            string userInput;
+            bool run = true;
+
+            WriteLine(outputString);
+
+            while (run)
+            {
+                userInput = ReadLine();
+                if (userInput == "exit")
+                {
+                    run = false;
+                }
+                else
+                {
+                    char[] inputAsChar = userInput.ToArray();
+                    ConvertStringToPigLatin(inputAsChar);
+                }
+            }
+        }
+
+        public static void ConvertStringToPigLatin(char[] inputArray)
+        {
+            char temp;
+            temp = inputArray[0];
+
+            if (temp != 'a' && temp != 'e' && temp != 'i' && temp != 'o' && temp != 'u' && temp != 'y')
+            {
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    if (i + 1 != inputArray.Length)
+                    {
+                        inputArray[i] = inputArray[i + 1];
+                    }
+                }
+                inputArray[inputArray.Length - 1] = temp;
+            }
+
+            Array.Resize(ref inputArray, inputArray.Length + 2);
+            inputArray[inputArray.Length - 2] = 'a';
+            inputArray[inputArray.Length - 1] = 'y';
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                WriteLine(inputArray[i]);
+            }
+        }
+    }
+}
